beabb6fb1bf1f29c019a6fde60c57d66e8f82d2e
sledgej <jonathan.sledge@schencksc.com>
2017-05-11 09:55:41 -0500

    initial creation of client search component

--- src/app/client-search/client-search.component.ts
+++ src/app/client-search/client-search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+import { ClientSearchService } from './services/client-search.service';
+import { Client } from '../client';
 
 @Component({
   selector: 'app-client-search',
@@ -6,10 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-search.component.css']
 })
 export class ClientSearchComponent implements OnInit {
+  clientSearch: FormGroup;
+  searchClientName: Boolean;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private clientSearchService: ClientSearchService) { }
 
   ngOnInit() {
+    this.clientSearch = this.fb.group({
+      ClientId: 0,
+      ClientName: ''
+    });
   }
 
 }
