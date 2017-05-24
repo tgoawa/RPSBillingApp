6d0d63c3091b2d43adba636dc4550c6c6d4304f0
sledgej <jonathan.sledge@schencksc.com>
2017-05-24 08:38:47 -0500

    get function added to fee service

--- src/app/maintenance/maintenance-entry/services/maintenance-fee.service.ts
+++ src/app/maintenance/maintenance-entry/services/maintenance-fee.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 
+import 'rxjs/add/operator/map';
+import { environment } from '../../../../environments/environment';
+import { RpsClientFee } from '../../../client';
+
+const api = environment.envApi;
 @Injectable()
 export class MaintenanceFeeService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  getRPSFee(clientId: number) {
+    return this.http.get(api + 'GetRPSFee/' + clientId)
+    .map(response => response.json(), error => console.log(error));
+  }
 
 }
