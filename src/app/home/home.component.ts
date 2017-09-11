7946f0309ddeabaec6ba19a7812b3440770f6444
Jonathan Sledge <jonathan.sledge@schencksc.com>
2017-09-11 10:36:28 -0500

    added import button and basic functionality for csv import

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,4 +12,38 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
+  onCsvImport(evt) {
+    const files = evt.target.files;
+    const file = files[0];
+    const reader = new FileReader();
+
+    reader.readAsText(file);
+    reader.onload = (event: any) => {
+      console.log(event);
+      const csv = event.target.result;
+      this.extractData(csv);
+    };
+  }
+
+  private extractData(data) {
+
+    const csvData = data;
+    const allTextLines = csvData.split(/\r\n|\n/);
+    const headers = allTextLines[0].split(',');
+    const lines = [];
+
+    for (let i = 1; i < allTextLines.length; i++) {
+      // split content based on comma
+      const allData = allTextLines[i].split(',');
+      if (allData.length === headers.length) {
+        const tarr = [];
+        for (let j = 0; j < headers.length; j++) {
+          tarr.push(allData[j]);
+        }
+        lines.push(tarr);
+      }
+    }
+    console.log(lines);
+  }
+
 }
